b4d0292d948a3f43adc8a7bf8c2b1ec435bfc4e8
Bruno Paulino <brunojppb@gmail.com>
2017-07-20 23:45:43 +0200

    using WS service to make HTTP Calls

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,28 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import model.SunInfo.SunInfo
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
-class Application @Inject() (components: ControllerComponents)
+import scala.concurrent.ExecutionContext
+
+
+class Application @Inject() (components: ControllerComponents, ws: WSClient)(implicit ec: ExecutionContext)
     extends AbstractController(components) {
-  def index = Action {
-    Ok(views.html.index())
+
+  def index = Action.async {
+    ws.url("http://api.sunrise-sunset.org/json")
+      .withQueryStringParameters("lat" -> "33.8830", "lng" -> "151.2167", "formatted" -> "0")
+      .get()
+      .map {
+        response =>
+          val sunriseTime = (response.json \ "results" \ "sunrise").as[String]
+          val sunsetTime = (response.json \ "results" \ "sunset").as[String]
+          val sunInfo = SunInfo(sunriseTime, sunsetTime)
+          Ok(views.html.index(sunInfo))
+      }
   }
+
 }
---
+++ app/model/SunInfo/SunInfo.scala
@@ -0,0 +1,3 @@
+package model.SunInfo
+
+case class SunInfo(sunrise: String, sunset: String)
