59b6ab5935fdd58d548c0f2ae8acd70cb34c8053
Bruno Paulino <brunojppb@gmail.com>
2017-07-21 00:02:07 +0200

    Chaining api calls with for comprehension

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,19 +15,28 @@ class Application @Inject() (components: ControllerComponents, ws: WSClient)(imp
     extends AbstractController(components) {
 
   def index = Action.async {
-    ws.url("http://api.sunrise-sunset.org/json")
+    val dayTimeData = ws.url("http://api.sunrise-sunset.org/json")
       .withQueryStringParameters("lat" -> "33.8830", "lng" -> "151.2167", "formatted" -> "0")
       .get()
-      .map {
-        response =>
-          val sunriseTime = (response.json \ "results" \ "sunrise").as[String]
-          val sunsetTime = (response.json \ "results" \ "sunset").as[String]
-          val sunr = ZonedDateTime.parse(sunriseTime)
-          val suns = ZonedDateTime.parse(sunsetTime)
-          val formatter = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(ZoneId.of("Australia/Sydney"))
-          val sunInfo = SunInfo(sunr.format(formatter), suns.format(formatter))
-          Ok(views.html.index(sunInfo))
-      }
+
+    val weatherData = ws.url("http://api.openweathermap.org/data/2.5/weather")
+      .withQueryStringParameters("lat" -> "33.8830", "lon" -> "151.2167", "units" -> "metric", "appId" -> "7d2b7bf00009f7e3371058073b7c7f76")
+      .get()
+
+    for {
+      dayTime <- dayTimeData
+      weather <- weatherData
+    } yield {
+      val sunriseTime = (dayTime.json \ "results" \ "sunrise").as[String]
+      val sunsetTime = (dayTime.json \ "results" \ "sunset").as[String]
+      val sunr = ZonedDateTime.parse(sunriseTime)
+      val suns = ZonedDateTime.parse(sunsetTime)
+      val formatter = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(ZoneId.of("Australia/Sydney"))
+      val sunInfo = SunInfo(sunr.format(formatter), suns.format(formatter))
+      val temperature = (weather.json \ "main" \ "temp").as[Double]
+      Ok(views.html.index(sunInfo, temperature))
+    }
+
   }
 
 }
